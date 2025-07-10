0be0e665bc745b8432180c37552890fd0a204351
Vaishnavi Chirmade <vaishnavi.chirmade@gmail.com>
2025-07-10 10:02:42 +0530

    Linux_Shell_Scripting

---
+++ check_if_leap_year_or_not.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter Year : "
+read year
+
+if (( year % 4 == 0 && year % 100 != 0 || year % 400 == 0 )); 
+then
+    echo "$year is a Leap Year!"
+else
+    echo "$year is Not a Leap Year."
+
+fi
